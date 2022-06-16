95eac63fe9aa81f0a66ee2c65ce9ac52aa2d1b6e
Félix Baylac-Jacqué <felix@alternativebit.fr>
2022-06-16 17:27:56 +0200

    h-forge-fetchers: add git.sr.ht to the default fetchers

--- h.el
+++ h.el
@@ -79,7 +79,9 @@ Errors out if we can't find it."
      ((query-user-repo . h--query-github-owner-repo)
       (url . "https?://github.com/.*")))
     ("GitLab.com" .
-     ((query-user-repo . (lambda (owner repo cb) (h--query-gitlab-owner-repo "gitlab.com" owner repo cb))))))
+     ((query-user-repo . (lambda (owner repo cb) (h--query-gitlab-owner-repo "gitlab.com" owner repo cb)))))
+    ("git.sr.ht" .
+     ((query-user-repo . (lambda (owner repo cb) (h--query-sourcehut-owner-repo "git.sr.ht" owner repo cb))))))
 
   "Fetchers meant to be used in conjunction with ‘h-forge-fetchers’.
 
